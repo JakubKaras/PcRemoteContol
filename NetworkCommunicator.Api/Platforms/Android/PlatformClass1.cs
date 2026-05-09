52b47ed27dd87a7fe78395b3c2f9a6656ebbff8d
JakubKaras <karas.kuba@email.cz>
2026-05-09 21:42:24 +0200

    updated app to .NET 10, removed boilerplate files

--- NetworkCommunicator.Api/Platforms/Android/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NetworkCommunicator.Api
-{
-    // All the code in this file is only included on Android.
-    public class PlatformClass1
-    {
-    }
-}
--- NetworkCommunicator.Api/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NetworkCommunicator.Api
-{
-    // All the code in this file is only included on Mac Catalyst.
-    public class PlatformClass1
-    {
-    }
-}
